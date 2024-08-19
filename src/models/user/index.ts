876ee36dc9348fa80e324df0fd734718c5a7fcf4
diegorezm <dmourarezende@gmail.com>
2024-08-19 16:50:15 -0300

    models

---
+++ src/models/user/index.ts
@@ -0,0 +1,23 @@
+import { createInsertSchema, createSelectSchema } from "drizzle-zod";
+import { userTableSchema } from "../../db/schema";
+import { z } from "zod";
+
+// get user schema from the table
+const userSelectSchema = createSelectSchema(userTableSchema).omit({
+  password: true,
+});
+
+export const userInsertSchema = createInsertSchema(userTableSchema).omit({
+  createdAt: true,
+  updatedAt: true,
+});
+
+export const userLoginSchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+// export user schema as User type
+export type User = z.infer<typeof userSelectSchema>;
+export type UserDTO = z.infer<typeof userInsertSchema>;
+export type UserLoginDTO = z.infer<typeof userLoginSchema>;
