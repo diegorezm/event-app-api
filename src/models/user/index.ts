0e1e45c348bac1bc69a8d543698b7d8aceea1b83
rafaels549 <rafalesantoscostat@gmail.com>
2024-08-22 16:05:49 -0300

    UptadateSchema

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -17,7 +17,15 @@ export const userLoginSchema = z.object({
   password: z.string(),
 });
 
+export const userUpdateSchema = createInsertSchema(userTableSchema)
+  .omit({
+    createdAt: true,
+    updatedAt: true,
+  })
+  .partial();
+
 // export user schema as User type
 export type User = z.infer<typeof userSelectSchema>;
 export type UserDTO = z.infer<typeof userInsertSchema>;
 export type UserLoginDTO = z.infer<typeof userLoginSchema>;
+export type UserUpdateDTO = z.infer<typeof userUpdateSchema>;
