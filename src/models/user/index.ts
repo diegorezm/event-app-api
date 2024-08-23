f2ccc570f30e1904b28f8cf4182bb4059993d9a9
diegorezm <dmourarezende@gmail.com>
2024-08-23 16:33:38 -0300

    fixed user insert schema

--- src/models/user/index.ts
+++ src/models/user/index.ts
@@ -2,25 +2,32 @@ import { createInsertSchema, createSelectSchema } from "drizzle-zod";
 import { userTableSchema } from "../../db/schema";
 import { z } from "zod";
 
-
 const userSelectSchema = createSelectSchema(userTableSchema).omit({
   password: true,
 });
 
-export const userInsertSchema = createInsertSchema(userTableSchema).omit({
-  createdAt: true,
-  updatedAt: true,
-})
-.extend({
-  name: z.string({required_error:"Nome é obrigatório"}),
-  email: z.string({ required_error: "E-mail é obrigatório" }).email("E-mail inválido"),
-  password: z.string({ required_error: "Senha é obrigatória" })
+export const userInsertSchema = createInsertSchema(userTableSchema, {
+  name: z.string({ required_error: "Nome é obrigatório" }),
+  email: z
+    .string({ required_error: "E-mail é obrigatório" })
+    .email("E-mail inválido"),
+  password: z
+    .string({ required_error: "Senha é obrigatória" })
     .min(8, { message: "Senha tem que ter no minímo 8 caracteres" }),
-  confirmPassword: z.string({ required_error: "Por favor confirme sua senha" }),
-}).refine((data) => data.password === data.confirmPassword, {
-  message: "Senhas não conhecidem",
-  path: ["confirmPassword"], 
-});;
+})
+  .omit({
+    createdAt: true,
+    updatedAt: true,
+  })
+  .extend({
+    confirmPassword: z.string({
+      required_error: "Por favor confirme sua senha",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Senhas não conhecidem",
+    path: ["confirmPassword"],
+  });
 
 export const userLoginSchema = z.object({
   email: z.string().email("E-mail inválido"),
