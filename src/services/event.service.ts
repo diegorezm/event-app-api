f551f003dfb87756121aebfaa5d0924b1f752fb1
diegorezm <dmourarezende@gmail.com>
2024-09-25 12:11:29 -0300

    event service

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,9 +1,37 @@
-class EventService {
-  async getAll() {}
-  async getById() {}
-  async create() {}
-  async update() {}
-  async delete() {}
+import { EventDTO, Event } from "../models/events";
+import { IEventRepository } from "../repositories/event.repository";
+import { NotFoundError, PaginatedRequest, PaginatedResponse } from "../types";
+
+interface IEventService {
+  findAll(params: PaginatedRequest): Promise<PaginatedResponse<Event>>
+  findById(id: string): Promise<Event>
+  create(event: EventDTO): Promise<Event>
+  update(id: string, event: EventDTO): Promise<Event>
+  delete(id: string): Promise<void>
 }
 
-export default new EventService();
+export class EventService implements IEventService {
+  constructor(private readonly eventRepository: IEventRepository) { }
+  async findAll(params: PaginatedRequest): Promise<PaginatedResponse<Event>> {
+    const events = await this.eventRepository.findAll(params)
+    return events
+  }
+  async findById(id: string): Promise<Event> {
+    const event = await this.eventRepository.findById(id)
+    if (!event) {
+      throw new NotFoundError("Evento não encontrado")
+    }
+    return event
+  }
+  async create(event: EventDTO): Promise<Event> {
+    const eventCreated = await this.eventRepository.create(event)
+    return eventCreated
+  }
+  async update(id: string, event: EventDTO): Promise<Event> {
+    const eventUpdated = await this.eventRepository.update(id, event)
+    return eventUpdated
+  }
+  async delete(id: string): Promise<void> {
+    return this.eventRepository.delete(id)
+  }
+}
